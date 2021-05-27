8e8992ecb395359ead8f21013e8d288ea5c3d55d
Mariam Abdelrazek <mariamahmedy4@gmail.com>
2021-05-28 01:33:05 +0200

    Style login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -24,27 +24,29 @@ class Login extends Component {
     const { UsersIds, users } = this.props;
 
     return (
-      <div className="center">
-        <h1>Please Choose a User</h1>
-        <form onSubmit={this.handleSubmit}>
-          <select onChange={this.onChange} defaultValue="selectUser">
-            <option disabled value="selectUser">
-              Select User
-            </option>
-            {UsersIds.map((id) => (
-              <option key={id} value={id}>
-                {users[id].name}
+      <div className="container">
+        <div className="center">
+          <h1>Please Choose a User</h1>
+          <form onSubmit={this.handleSubmit}>
+            <select onChange={this.onChange} defaultValue="selectUser">
+              <option disabled value="selectUser">
+                Select User
               </option>
-            ))}
-          </select>
-          <button
-            className="button"
-            type="submit"
-            disabled={this.state.user === ""}
-          >
-            Login
-          </button>
-        </form>
+              {UsersIds.map((id) => (
+                <option key={id} value={id}>
+                  {users[id].name}
+                </option>
+              ))}
+            </select>
+            <button
+              className="button"
+              type="submit"
+              disabled={this.state.user === ""}
+            >
+              Login
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
